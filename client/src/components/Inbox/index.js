0e4cb6e9fe264569e4b141bafabdd9b713d41779
Joaobarbosa95 <bticjoaobarbosa@hotmail.com>
2021-12-09 18:02:26 +0000

    inbox component inital draft

---
+++ client/src/components/Inbox/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./inbox.css";
+
+const index = () => {
+  return (
+    <div className="inbox-container">
+      <div className="dialogues-container">a</div>
+      <div className="direct-messages-container">b</div>
+      <div className="about-user-container">c</div>
+    </div>
+  );
+};
+
+export default index;
