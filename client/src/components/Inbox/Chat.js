d4f7dcd854b040412e3b79b5e2fc0d41da4cf539
Joaobarbosa95 <bticjoaobarbosa@hotmail.com>
2021-12-18 14:29:29 +0000

    private messages graphic display

--- client/src/components/Inbox/Chat.js
+++ client/src/components/Inbox/Chat.js
@@ -1,20 +1,44 @@
 import React from "react";
 import ChatForm from "../Users/GlobalChat/Chat/Chat";
+import { FaUserCircle } from "react-icons/fa";
 
+import "./chat.css";
 const Chat = () => {
+  const user = { avatar: null };
+
   return (
     <>
-      <div className="user">
-        <img src="" alt="avatar" />
-        <p className="chat-username">name</p>
-        <p className="chat-status">online 3H ago</p>
-      </div>
-      <div className="messages-box">
-        <div className="message">
-          <div className="individual-message-text"></div>
-          <div className="message-time"></div>
+      <div className="chat-user">
+        {user.avatar ? (
+          <img src={user.avatar} alt="avatar" className="avatar" />
+        ) : (
+          <FaUserCircle className="avatar" />
+        )}
+        <div className="user-info">
+          <div className="chat-username">username</div>
+          <div className="options">options</div>
+          <div className="chat-status">Online</div>
         </div>
       </div>
+      <div className="messages-box">
+        <div className="message-receiver">
+          <div className="individual-message-receiver">
+            {" "}
+            Olá chicquitoaweeeeeeeeeeeeeeeeeeeeeeeeeeeeebbbbbbbbbbbbbbbbbb
+            awerrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrawer aweraw
+          </div>
+          <div className="message-time">13-10-2020</div>
+        </div>
+        <div className="message-sender">
+          <div className="individual-message-sender">
+            {" "}
+            Olá chicquitoaweeeeeeeeeeeeeeeeeeeeeeeeeeeeebbbbbbbbbbbbbbbbbb
+            awerrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrawer aweraw
+          </div>
+          <div className="message-time sender">13-10-2020</div>
+        </div>
+      </div>
+
       <ChatForm />
     </>
   );
