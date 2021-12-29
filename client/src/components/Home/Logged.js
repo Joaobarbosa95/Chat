e21237216f48615ca9119af421574ec94326c479
Joaobarbosa95 <bticjoaobarbosa@hotmail.com>
2021-12-29 19:01:30 +0000

    logout added and navigation routes handled

--- client/src/components/Home/Logged.js
+++ client/src/components/Home/Logged.js
@@ -1,10 +1,32 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useUserContext } from "../Contexts/UserContext";
+import { useLocation, useNavigate } from "react-router-dom";
+import { logout } from "../../services/api/user";
+import { useAuthContext } from "../Contexts/AuthContext";
 
 export const Logged = () => {
   const { user } = useUserContext();
+  const { setAuthed } = useAuthContext();
+  const location = useLocation();
+  const navigate = useNavigate();
 
-  return <div>You are logge as {user.username}</div>;
+  useEffect(() => {
+    if (location.state) navigate(location.state.from.pathname);
+  }, []);
+
+  return (
+    <div>
+      You are logge as {user.username}{" "}
+      <button
+        onClick={() => {
+          logout(user);
+          setAuthed(false);
+        }}
+      >
+        Logout
+      </button>
+    </div>
+  );
 };
 
 export default Logged;
